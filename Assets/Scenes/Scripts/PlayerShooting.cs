e17c5eaa3057d6946aff1a8cc6cfd3ddaee0b658
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-21 17:18:29 +0000

    refactoring player shooting script

--- Assets/Scenes/Scripts/PlayerShooting.cs
+++ Assets/Scenes/Scripts/PlayerShooting.cs
@@ -6,13 +6,17 @@ public class PlayerShooting : MonoBehaviour
 {
     public GameObject bulletPrefab;
 
-    public float fireDelay = 0.25f;
-    public float cooldownTimer = 0;
+    public float fireDelay;
+    public float cooldownTimer;
+    public float killPoint = 100f;
+    private int meteorsCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        fireDelay = 0.25f;
+        cooldownTimer = 0;
+        meteorsCount = GameObject.FindGameObjectsWithTag("Meteor").Length;
     }
 
     // Update is called once per frame
@@ -20,7 +24,7 @@ public class PlayerShooting : MonoBehaviour
     {
         cooldownTimer -= Time.deltaTime;
 
-        if (cooldownTimer <= 0)
+        if (cooldownTimer <= 0 && transform.position.y <= 10)
         {
             cooldownTimer = fireDelay;
 
@@ -32,6 +36,13 @@ public class PlayerShooting : MonoBehaviour
 
     void OnGUI()
     {
-        GUI.Label(new Rect(0, 0, 100, 50), "Objetos restantes: " + GameObject.FindGameObjectsWithTag("Objects").Length);
+        float points = (meteorsCount - GameObject.FindGameObjectsWithTag("Meteor").Length) * killPoint;
+
+        GUI.Label(new Rect(0, 0, 100, 50), "Puntos: " + points);
+
+        if (transform.position.y >= 10)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 25, 100, 50), "You win! \n Points: " + points);
+        }
     }
 }

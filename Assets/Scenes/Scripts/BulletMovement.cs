40e1dc57de30be20da10b6be726d08930e587489
Raúl Marrero Quevedo <ramaque01@gmail.com>
2023-12-21 17:17:43 +0000

    refactoring bullet movement script

--- Assets/Scenes/Scripts/BulletMovement.cs
+++ Assets/Scenes/Scripts/BulletMovement.cs
@@ -6,12 +6,6 @@ public class BulletMovement : MonoBehaviour
 {
     public float maxSpeed = 3.5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
